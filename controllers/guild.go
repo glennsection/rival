a05ed1488b519513dd995cb1553a41193436455d
LASER\michaelbedernik <mike.bedernik@sectionstudios.com>
2017-07-05 17:49:51 -0700

    [BT-30] Guild Creat and join

--- controllers/guild.go
+++ controllers/guild.go
@@ -1,13 +1,19 @@
 package controllers
 
 import (
-	"bloodtales/system"
 	"bloodtales/models"
+	"bloodtales/system"
 	"bloodtales/util"
+
+	"gopkg.in/mgo.v2/bson"
+
+	"fmt"
 )
 
 func handleGuild() {
 	handleGameAPI("/guild/create", system.TokenAuthentication, CreateGuild)
+	handleGameAPI("/guild/getGuilds", system.TokenAuthentication, GetGuilds)
+	handleGameAPI("/guild/addMember", system.TokenAuthentication, AddMember)
 }
 
 func CreateGuild(context *util.Context) {
@@ -22,4 +28,40 @@ func CreateGuild(context *util.Context) {
 	// create guild
 	_, err := models.CreateGuild(context, player, name)
 	util.Must(err)
+}
+
+func GetGuilds(context *util.Context) {
+	// parse parameters
+	name := context.Params.GetString("name", "")
+
+	var guilds []*models.Guild
+	if name != "" {
+		util.Must(context.DB.C(models.GuildCollectionName).Find(bson.M{
+			"nm": bson.M{
+				"$regex": bson.RegEx{
+					Pattern: fmt.Sprintf(".*%s.*", name),
+					Options: "i",
+				},
+			},
+		}).All(&guilds))
+	} else {
+		util.Must(context.DB.C(models.GuildCollectionName).Find(nil).All(&guilds))
+	}
+	//util.Must(context.DB.C(models.GuildCollectionName).Find(bson.M{"nm": bson.M{"$eq": "MikeMasterGuild"}}).All(&guilds))
+
+	// result
+	context.SetData("guilds", guilds)
+}
+
+func AddMember(context *util.Context) {
+	tag := context.Params.GetRequiredString("tag")
+
+	player := GetPlayer(context)
+
+	// guild
+	guild, err := models.GetGuildByTag(context, tag)
+	util.Must(err)
+
+	err2 := models.AddMember(context, player, guild)
+	util.Must(err2)
 }
--- models/guild.go
+++ models/guild.go
@@ -4,13 +4,14 @@ import (
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 
-	"bloodtales/util"
 	"bloodtales/data"
+	"bloodtales/util"
 )
 
 const GuildCollectionName = "guilds"
 
 type GuildRole int
+
 const (
 	GuildMember GuildRole = iota
 	GuildElder
@@ -19,22 +20,23 @@ const (
 )
 
 type Guild struct {
-	ID              	bson.ObjectId   `bson:"_id,omitempty" json:"-"`
-	OwnerID        	 	bson.ObjectId   `bson:"ow" json:"-"`
-	Name                string          `bson:"nm" json:"name"`
-	XP                  int             `bson:"xp" json:"xp"`
-	Rating          	int             `bson:"rt" json:"rating"`
-	MemberCount         int             `bson:"ms" json"-"`
+	ID          bson.ObjectId `bson:"_id,omitempty" json:"-"`
+	OwnerID     bson.ObjectId `bson:"ow" json:"-"`
+	Name        string        `bson:"nm" json:"name"`
+	Tag         string        `bson:"tg" json:"tag"`
+	XP          int           `bson:"xp" json:"xp"`
+	Rating      int           `bson:"rt" json:"rating"`
+	MemberCount int           `bson:"ms" json:"-"`
 
-	WinCount       		int             `bson:"wc" json:"winCount"`
-	LossCount       	int             `bson:"lc" json:"lossCount"`
-	MatchCount       	int             `bson:"mc" json:"matchCount"`
+	WinCount   int `bson:"wc" json:"winCount"`
+	LossCount  int `bson:"lc" json:"lossCount"`
+	MatchCount int `bson:"mc" json:"matchCount"`
 }
 
 // client model
 type GuildClientAlias Guild
 type GuildClient struct {
-	Members             []*PlayerClient `json:"members"`
+	Members []*PlayerClient `json:"members"`
 
 	*GuildClientAlias
 }
@@ -42,7 +44,7 @@ type GuildClient struct {
 func (guild *Guild) CreateGuildClient(context *util.Context) (client *GuildClient, err error) {
 	// get member players
 	var memberPlayers []*Player
-	err = context.DB.C(PlayerCollectionName).Find(bson.M { "gd": guild.ID } ).All(&memberPlayers)
+	err = context.DB.C(PlayerCollectionName).Find(bson.M{"gd": guild.ID}).All(&memberPlayers)
 	if err != nil {
 		return
 	}
@@ -60,7 +62,7 @@ func (guild *Guild) CreateGuildClient(context *util.Context) (client *GuildClien
 	}
 
 	// create client model
-	client = &GuildClient {
+	client = &GuildClient{
 		Members: members,
 
 		GuildClientAlias: (*GuildClientAlias)(guild),
@@ -72,8 +74,8 @@ func ensureIndexGuild(database *mgo.Database) {
 	c := database.C(GuildCollectionName)
 
 	// owner index
-	util.Must(c.EnsureIndex(mgo.Index {
-		Key:        []string { "ow" },
+	util.Must(c.EnsureIndex(mgo.Index{
+		Key:        []string{"ow"},
 		Unique:     true,
 		DropDups:   true,
 		Background: true,
@@ -83,13 +85,19 @@ func ensureIndexGuild(database *mgo.Database) {
 
 func GetGuildById(context *util.Context, id bson.ObjectId) (guild *Guild, err error) {
 	// find guild by ID
-	err = context.DB.C(GuildCollectionName).Find(bson.M { "_id": id } ).One(&guild)
+	err = context.DB.C(GuildCollectionName).Find(bson.M{"_id": id}).One(&guild)
+	return
+}
+
+func GetGuildByTag(context *util.Context, tag string) (guild *Guild, err error) {
+	// find guild by ID
+	err = context.DB.C(GuildCollectionName).Find(bson.M{"tg": tag}).One(&guild)
 	return
 }
 
 func GetGuildByOwner(context *util.Context, ownerId bson.ObjectId) (guild *Guild, err error) {
 	// find guild by owner ID
-	err = context.DB.C(GuildCollectionName).Find(bson.M { "ow": ownerId } ).One(&guild)
+	err = context.DB.C(GuildCollectionName).Find(bson.M{"ow": ownerId}).One(&guild)
 	return
 }
 
@@ -103,13 +111,14 @@ func (guild *Guild) initialize() {
 
 func CreateGuild(context *util.Context, owner *Player, name string) (guild *Guild, err error) {
 	// init guild
-	guild = &Guild {}
+	guild = &Guild{}
 	guild.initialize()
 
 	// set owner and name
 	guild.OwnerID = owner.ID
 	guild.Name = name
 	guild.MemberCount = 1
+	guild.Tag = util.GenerateTag()
 
 	// save guild
 	err = guild.Save(context)
@@ -130,19 +139,30 @@ func CreateGuild(context *util.Context, owner *Player, name string) (guild *Guil
 	return
 }
 
+func AddMember(context *util.Context, player *Player, guild *Guild) (err error) {
+	guild.MemberCount++
+	err = guild.Save(context)
+
+	player.GuildID = guild.ID
+	player.GuildRole = GuildMember
+	player.Save(context)
+	player.SetDirty(PlayerDataMask_Guild)
+	return
+}
+
 func (guild *Guild) Save(context *util.Context) (err error) {
 	if !guild.ID.Valid() {
 		guild.ID = bson.NewObjectId()
 	}
 
 	// update entire guild to database
-	_, err = context.DB.C(GuildCollectionName).Upsert(bson.M { "_id": guild.ID }, guild)
+	_, err = context.DB.C(GuildCollectionName).Upsert(bson.M{"_id": guild.ID}, guild)
 	return
 }
 
 func (guild *Guild) Delete(context *util.Context) (err error) {
 	// delete guild from database
-	return context.DB.C(GuildCollectionName).Remove(bson.M { "_id": guild.ID })
+	return context.DB.C(GuildCollectionName).Remove(bson.M{"_id": guild.ID})
 }
 
 func (guild *Guild) GetLevel() int {
