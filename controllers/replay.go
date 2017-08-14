4c01808b1ebf94a2fc7ba76625727d86762098b3
Dustin <dustin@sectionstudios.com>
2017-08-14 15:54:35 -0700

    Adding top replay list

--- controllers/replay.go
+++ controllers/replay.go
@@ -9,6 +9,8 @@ import (
 func handleReplay() {
 	handleGameAPI("/replay/list", system.TokenAuthentication, GetReplayList)
 	handleGameAPI("/replay/get", system.TokenAuthentication, GetReplay)
+	handleGameAPI("/replay/getLastReplay", system.TokenAuthentication, GetLastReplayInfoByUser)
+	handleGameAPI("/replay/getTopReplays", system.TokenAuthentication, GetTopReplays)
 	handleGameAPI("/replay/set", system.TokenAuthentication, SetReplay)
 	handleGameAPI("/replay/delete", system.TokenAuthentication, DeleteReplay)
 }
@@ -18,6 +20,25 @@ func GetReplayList(context *util.Context) {
 	util.Must(err)
 
 	context.SetData("replays", replayInfos)
+}
+
+func GetTopReplays(context *util.Context) {
+	replayInfos, err := models.GetTopReplays(context, context.UserID)
+	util.Must(err)
+
+	context.SetData("replays", replayInfos)
+}
+
+func GetLastReplayInfoByUser(context *util.Context) {
+	playerTag := context.Params.GetRequiredString("tag")
+
+	player,err := models.GetPlayerByTag(context, playerTag)
+	util.Must(err)
+
+	replayInfo, err2 := models.GetLastReplayInfoByUser(context, player.UserID)
+	util.Must(err2)
+
+	context.SetData("replay", replayInfo)
 }
 
 func GetReplay(context *util.Context) {
@@ -34,8 +55,9 @@ func SetReplay(context *util.Context) {
 	// parse parameters
 	info := context.Params.GetRequiredString("info")
 	data := context.Params.GetRequiredString("data")
+	rank := context.Params.GetRequiredInt("rank")
 
-	util.Must(models.CreateReplay(context, info, data))
+	util.Must(models.CreateReplay(context, info, data, rank))
 }
 
 func DeleteReplay(context *util.Context) {
